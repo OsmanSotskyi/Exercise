2009f15cb3ed3f9c292f8fa80347d3ad01813b14
OsmanSotskyi <sockiy.o@gmail.com>
2024-05-30 01:05:29 +0100

    second commit

--- src/test/java/words/Words.java
+++ src/test/java/words/Words.java
@@ -6,19 +6,14 @@ import java.util.List;
 import java.util.Set;
 
 public class Words {
-
-
         public static List<String> getUniqueWordsFromSentence(String sentence) {
-            // conv the sentences to lowercase
+
             String lowerCaseSentence = sentence.toLowerCase();
 
-            // Remove punctuation by replacing all non-word characters (except spaces) with space
             String cleanedSentence = lowerCaseSentence.replaceAll("[^a-zA-Z\\s]", " ");
 
-            // Split the cleaned sentence into words
             String[] wordsArray = cleanedSentence.split("\\s+");
 
-            // Use a set to collect unique words
             Set<String> uniqueWordsSet = new HashSet<>();
             for (String word : wordsArray) {
                 if (!word.isEmpty()) {
@@ -26,10 +21,8 @@ public class Words {
                 }
             }
 
-            // Convert the set back to a list
             List<String> uniqueWordsList = new ArrayList<>(uniqueWordsSet);
-
-            // Return the list of unique words
+            
             return uniqueWordsList;
         }
     }
